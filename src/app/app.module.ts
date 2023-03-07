a7b9adbf4c4e0fa68bb18340181a3b988e1fb518
Göktay İncekara <goktayincekara@gmail.com>
2023-03-07 21:39:03 +0300

    Data bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './successAlert/successAlert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  disabled = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "";
 
+  constructor() {
+    setTimeout(() => {
+      this.disabled = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
+  }
+
+  /*
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  */
+ 
 }
